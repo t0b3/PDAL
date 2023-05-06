7a64023a18aae6f2939ae50d0de4552dc2bc1423
Thomas Bettler <thomas.bettler@gmail.com>
2023-05-06 10:16:08 +0200

    chore: unbundle eigen
    Related: https://github.com/PDAL/PDAL/issues/4041

--- cmake/eigen.cmake
+++ cmake/eigen.cmake
@@ -2,7 +2,7 @@
 # Eigen support
 #
 
-find_package(Eigen3 REQUIRED 3.1.2)
+find_package(Eigen3 3.4.0)
 set_package_properties(Eigen3 PROPERTIES TYPE REQUIRED)
 if (NOT Eigen3_FOUND)
     message(FATAL_ERROR "Eigen support is required")
