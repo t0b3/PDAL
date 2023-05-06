962258dbfbc93b3b7ac1b67b31a5a45da05b828c
Thomas Bettler <thomas.bettler@gmail.com>
2023-05-06 11:08:13 +0200

    fixup1

--- cmake/eigen.cmake
+++ cmake/eigen.cmake
@@ -2,8 +2,12 @@
 # Eigen support
 #
 
-find_package(Eigen3 3.4.0)
+find_package(Eigen3 3.4.0 REQUIRED)
 set_package_properties(Eigen3 PROPERTIES TYPE REQUIRED)
-if (NOT Eigen3_FOUND)
-    message(FATAL_ERROR "Eigen support is required")
+if (Eigen3_FOUND)
+    include_directories("${EIGEN3_INCLUDE_DIR}")
+    mark_as_advanced(CLEAR EIGEN3_INCLUDE_DIR)
+    mark_as_advanced(CLEAR EIGEN3_VERSION)
+else()
+    message(FATAL_ERROR "Eigen is required")
 endif()
